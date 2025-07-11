fec63b41a87b8b3c30740395840401ada4cc6ec4
Virus23-Dan <danieleedarnel@gmail.com>
2025-07-11 19:27:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,22 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  # ton HTML
+
+# Stocke la clé publique de chaque client
+public_keys = {}
+
+@socketio.on('public_key')
+def handle_public_key(data):
+    # Stocke la clé publique du client
+    public_keys[request.sid] = data['key']
+    # Envoie la clé publique à tous sauf l'expéditeur
+    emit('public_key', {'key': data['key']}, broadcast=True, include_self=False)
 
 @socketio.on('send_message')
 def handle_message(data):
-    message = data.get('message')
-    nickname = data.get('nickname')
-    emit('receive_message', {'message': message, 'nickname': nickname}, broadcast=True)
+    # Transmet le message chiffré à tout le monde
+    emit('receive_message', {'message': data['message'], 'nickname': data['nickname']}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
