b0c10d8e35ca3d987d07feaf61be46ba4b024519
Virus23-Dan <danieleedarnel@gmail.com>
2025-07-16 23:07:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import os
 
@@ -17,13 +17,16 @@ public_keys = {}
 def handle_public_key(data):
     # Stocke la clé publique du client
     public_keys[request.sid] = data['key']
-    # Envoie la clé publique à tous sauf l'expéditeur
+    # Envoie la clé publique à tous les autres clients sauf l'expéditeur
     emit('public_key', {'key': data['key']}, broadcast=True, include_self=False)
 
 @socketio.on('send_message')
 def handle_message(data):
-    # Transmet le message chiffré à tout le monde
-    emit('receive_message', {'message': data['message'], 'nickname': data['nickname']}, broadcast=True)
+    # Transmet le message chiffré à tous les clients
+    emit('receive_message', {
+        'message': data['message'], 
+        'nickname': data['nickname']
+    }, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
